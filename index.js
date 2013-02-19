ada566a6dbeac797f7d37a829f4b4f0ffe668579
Bill Casarin <bill@casarin.ca>
2013-02-19 15:58:22 -0500

    Shift fn from args

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var type = require('type');
 module.exports = function(fn){
   if (type(fn) !== 'function') return fn;
   var args = Array.prototype.slice.call(arguments);
+  args.shift();
   return fn.apply(this, args);
 };
 
