ce825bb606c0d78942b3a458f0d0730c98ea88f9
William Casarin <bill@casarin.me>
2014-03-27 11:52:39 -0700

    npmify and remove type component dep

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var type = require('type');
 
 module.exports = function(fn){
-  if (type(fn) !== 'function') return fn;
+  if (typeof fn !== 'function') return fn;
   var args = arguments.length > 1? [].slice.call(arguments, 1) : [];
   return fn.apply(this, args);
 };
-
